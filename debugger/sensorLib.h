25f46c07545b107948703e3c0eb590588a7cf634
BoraNy <49944183+BoraNy@users.noreply.github.com>
2022-12-28 22:43:57 +0700

    Added Sensor Library Template

---
+++ debugger/sensorLib.h
@@ -0,0 +1,24 @@
+#ifndef SENSOR_LIB_H
+#define SENSOR_LIB_H
+
+float readPHSensor()
+{
+  
+}
+
+float readTurbiditySensor()
+{
+  
+}
+
+float readOxygenSensor()
+{
+  
+}
+
+float readTemperatureSensor()
+{
+  
+}
+
+#endif
