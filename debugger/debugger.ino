a8e0a132886c065ec904bca7a04835fe06487ea1
BoraNy <49944183+BoraNy@users.noreply.github.com>
2022-12-30 09:13:00 +0700

    Added Full Sensor Support

--- debugger/debugger.ino
+++ debugger/debugger.ino
@@ -1,9 +1,27 @@
+#include "sensorLib.h"
+
+float tempC = 0,
+      turbidity = 0,
+      dissOxygen = 0,
+      PH = 0;
+
 void setup()
 {
   Serial.begin(115200);
+
+  sensorInitialization();
 }
 
 void loop()
 {
-  
+  turbidity = readTurbiditySensor(0);
+  tempC = readTemperatureSensor();
+  PH = readPHSensor(1);
+  dissOxygen = readOxygenSensor(2, tempC);
+
+  Serial.print("TempC: "); Serial.print(tempC); Serial.print('\t');
+  Serial.print("PH: "); Serial.print(PH); Serial.print('\t');
+  Serial.print("Turbidity: "); Serial.print(turbidity); Serial.print('\t');
+  Serial.print("Dissolve Oxygen: "); Serial.print(dissOxygen);
+  Serial.println();
 }
--- debugger/sensorLib.h
+++ debugger/sensorLib.h
@@ -1,24 +1,80 @@
 #ifndef SENSOR_LIB_H
 #define SENSOR_LIB_H
 
-float readPHSensor()
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+/* --- Temperature Sensor Parameters --- */
+#define DS18B20_PIN             4
+OneWire oneWire(DS18B20_PIN);
+DallasTemperature DS18B20(&oneWire);
+
+
+/* --- Oxygen Sensor Parameters --- */
+#define VREF                    5000
+#define ADC_RES                 1024
+#define TWO_POINT_CALIBRATION   0
+#define READ_TEMP               (25)
+#define CAL1_V                  (131)
+#define CAL1_T                  (25)
+#define CAL2_V                  (1300)
+#define CAL2_T                  (15)
+
+uint8_t Temperaturet;
+uint16_t ADC_Raw;
+uint16_t ADC_Voltage;
+uint16_t DO;
+
+const uint16_t DO_Table[41] = {
+  14460, 14220, 13820, 13440, 13090, 12740, 12420,
+  12110, 11810, 11530, 11260, 11010, 10770, 10530,
+  10300, 10080, 9860, 9660, 9460, 9270, 9080, 8900,
+  8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
+  7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730,
+  6630, 6530, 6410
+};
+
+void sensorInitialization()
 {
-  
+  DS18B20.begin();
 }
 
-float readTurbiditySensor()
+float readPHSensor(int analogPin)
 {
-  
+  float ph = analogRead(analogPin);
+  return ph * 0.017;
 }
 
-float readOxygenSensor()
+float readTurbiditySensor(int analogPin)
 {
-  
+  float v = analogRead(analogPin);
+  return (-1120.4 * sq(v) + 5742.3 * v - 4352.9);
 }
 
 float readTemperatureSensor()
 {
-  
+  DS18B20.requestTemperatures();
+  return DS18B20.getTempCByIndex(0);
+}
+
+/* --- Oxygen Sensor Functions --- */
+int16_t readDO(uint32_t voltage_mv, uint8_t temperature_c)
+{
+#if TWO_POINT_CALIBRATION == 00
+  uint16_t V_saturation = (uint32_t)CAL1_V + (uint32_t)35 * temperature_c - (uint32_t)CAL1_T * 35;
+  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
+#else
+  uint16_t V_saturation = (int16_t)((int8_t)temperature_c - CAL2_T) * ((uint16_t)CAL1_V - CAL2_V) / ((uint8_t)CAL1_T - CAL2_T) + CAL2_V;
+  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
+#endif
+}
+
+float readOxygenSensor(int analogPin, int temperature)
+{
+  Temperaturet = (uint8_t)temperature;
+  ADC_Raw = analogRead(analogPin);
+  ADC_Voltage = (uint32_t(VREF) * ADC_Raw / ADC_RES);
+  return readDO(ADC_Voltage, Temperaturet);
 }
 
 #endif
