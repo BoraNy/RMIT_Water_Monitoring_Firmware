0aefb42d760b65b01c10dcc329d5e805955c282a
aenonn <121560087+aenonn@users.noreply.github.com>
2023-01-18 00:00:38 +0700

    Testing with Temperature sensor and pH Sensor

--- Sub_Controller/Sub_Controller.ino
+++ Sub_Controller/Sub_Controller.ino
@@ -1,9 +1,27 @@
+#include "sensorLib.h"
+
+float tempC = 0,
+      turbidity = 0,
+      dissOxygen = 0,
+      PH = 0;
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  sensorInitialization();
 }
 
 void loop()
 {
-  Serial.print("Hello World");
+  turbidity = readTurbiditySensor(TURBIDITY_SENSOR_PIN);
+  tempC = readTemperatureSensor();
+  PH = readPHSensor(PH_SENSOR_PIN, tempC);
+  dissOxygen = readOxygenSensor(OXYGEN_SENSOR_PIN, tempC);
+
+  Serial.print("TempC: "); Serial.print(tempC); Serial.print('\t');
+  Serial.print("PH: "); Serial.print(PH); Serial.print('\t');
+  Serial.print("Turbidity: "); Serial.print(turbidity); Serial.print('\t');
+  Serial.print("Dissolve Oxygen: "); Serial.print(dissOxygen);
+  Serial.println();
 }
