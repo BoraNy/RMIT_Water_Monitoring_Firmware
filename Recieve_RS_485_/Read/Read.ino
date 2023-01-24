3e7822749d7dc0bb49de71837120453663ba97fe
Sokheang-Eang <eangsokheang23@gmail.com>
2023-01-25 03:44:39 +0700

    First_Coding

---
+++ Recieve_RS_485_/Read/Read.ino
@@ -0,0 +1,26 @@
+const uint8_t bufferlength = 50;
+char data2read [bufferlength];
+uint8_t charIndex = 0 ;
+char inByte;
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.begin(115200);
+}
+void loop()
+{
+  if (Serial1.available() > 0)
+  {
+    inByte = Serial1.read();
+    if (inByte != '\n' && (charIndex < bufferlength - 1)) {
+      data2read[charIndex] = inByte;
+      charIndex++;
+    }
+    else {
+      data2read[charIndex] = '\0';
+      Serial.print("Data: ");
+      Serial.println(data2read);
+      charIndex = 0;
+    }
+  }
+}
