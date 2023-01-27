9d3ac748777e12a9adaec49c83a2e908aefe4cc1
BoraNy <49944183+BoraNy@users.noreply.github.com>
2023-01-27 12:30:24 +0700

    Added Auto Serial Detection and Data Array

---
+++ Serial_Communication/Serial_Communication.ino
@@ -0,0 +1,27 @@
+float turbidity = 0,
+      pH = 0,
+      dissolveOxygen = 0,
+      temperature = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  turbidity = rand() % 50000 + rand() % 1000 * 1e-3;
+  pH = rand() % 14 + rand() % 1000 * 1e-3;
+  dissolveOxygen = rand() % 3000 + rand() % 1000 * 1e-3;
+  temperature = rand() % 150 + rand() % 1000 * 1e-3;
+
+  Serial.print(turbidity);
+  Serial.print(",");
+  Serial.print(pH);
+  Serial.print(",");
+  Serial.print(dissolveOxygen);
+  Serial.print(",");
+  Serial.print(temperature);
+  Serial.println();
+  delay(1000);
+}
---
+++ Serial_Communication/serialModule.py
@@ -0,0 +1,25 @@
+import serial
+import serial.tools.list_ports
+
+class SerialCommunication:
+    def __init__(self, baudrate) -> None:
+        self.hardwarePort = ''
+        self.ports = serial.tools.list_ports.comports()
+        for port in self.ports:
+            if 'Arduino' in port.description:
+                self.hardwarePort = port.device
+        self.hardwareSerial = serial.Serial(self.hardwarePort, baudrate, timeout=1000)
+    
+    def read(self):
+        data = str(self.hardwareSerial.readline())
+        data = data.replace('\\r\\n', '')
+        data = data.replace('b', '')
+        data = data.replace('\'', '')
+        return data.split(',')
+
+if __name__ == '__main__':
+    arduinoSerial = SerialCommunication(115200)
+    
+    while True:
+        print(arduinoSerial.read())
+        
